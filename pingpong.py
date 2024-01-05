44e20d53d63be32ec11f5cff7e39bed5b1e27ad1
KurtHertz <khert1@ocdsb.ca>
2024-01-05 16:53:01 -0500

    swich to class

--- pingpong.py
+++ pingpong.py
@@ -12,102 +12,99 @@ mip.install("github:mcauser/micropython-tm1637")
 import tm1637
 # Pinout for Wemos D1 mini:
 # https://static3.gleanntronics.ie/eng_pl_WeMos-D1-Mini-ESP8266-12F-Module-Arduino-IoT-426_2.jpg
-tm = tm1637.TM1637(clk=Pin(5), dio=Pin(4))
+#tm = tm1637.TM1637(clk=Pin(5), dio=Pin(4))
 
 
-
-def reset(score1, score2, chain1, chain2):
-    score1 = 0
-    score2 = 0
-    chain1 = 0
-    chain2 = 0
-    print("the game has been reset")
-    update_score(score1, score2, chain1, chain2)
-
-def update_score(score1, score2, chain1, chain2):
-    display(score1, score2)
-    special(score1, score2, chain1, chain2)
-
-def player1_scored(score1, score2, chain1, chain2):
-    score1 += 1
-    chain1 += 1
-    chain2 = 0
-    update_score(score1, score2, chain1, chain2)
-
-def player2_scored(score1, score2, chain1, chain2):
-    score2 += 1
-    chain2 += 1
-    chain1 = 0
-    update_score(score1, score2, chain1, chain2)
-
-def display(score1, score2):
-    global tm
-    tm.numbers(score1, score2)
-
-def special(score1, score2, chain1, chain2):
-    if(score1 == 21 and score2 < 20):
-        print("Home team wins")
-        reset()
-    elif(score2 == 21 and score1 < 20):
-        print("Away team wins")
-        reset()
-    elif(score1 > 19 and score1 == score2):
-        print("deuce")
-    elif(score1 == score2 + 2 and score2 > 19):
-        print("Home team wins")
-        reset()
-    elif(score2 == score1 + 2 and score1 > 19):
-        print("Away team wins")
-        reset()
-    elif(score1 == score2 + 1 and score2 > 19):
-        print("Advantege Home team")
-    elif(score2 == score1 + 1 and score1 > 19):
-        print("Advantege Away team")
-    elif(score1 == score2):
-        print("tie game")
-    elif(score1 == 20):
-        print("Home team is about to win")
-    elif(score2 == 20):
-        print("Away team is about to win")
-    elif(score1 > score2 + 10 and chain1 != 0):
-        print("Home team is destroying Away team")
-    elif(score2 > score1 + 10 and chain2 != 0):
-        print("Away team is destroying Home team")
-    
-    if(score1 > score2 + 2 and chain2 == 5):
-        print("Away team is on come back")
-    elif(score2 > score1 + 2 and chain1 == 5):
-        print("Home team is on come back")
-    if(chain1 == 3):
-        print("Home team is heating up")
-    elif(chain2 == 3):
-        print("Away team is heating up")
-    elif(chain1 == 5):
-        print("Home team is on fire")
-    elif(chain2 == 5):
-        print("Away team is on fire")
-    elif(chain1 == 7):
-        print("Home team needs to be stoped")
-    elif(chain2 == 7):
-        print("Away team needs to be stoped")
-    
+class Scoreboard:
+    def __init__(self, maxScore, winby):
+        self.score1 = 0
+        self.score2 = 0
+        self.chain1 = 0
+        self.chain2 = 0
+        self.winby = winby
+        self.maxScore = maxScore
+        self.tm = tm1637.TM1637(clk=Pin(5), dio=Pin(4))
+    def reset(self):
+        self.score1 = 0
+        self.score2 = 0
+        self.chain1 = 0
+        self.chain2 = 0
+        print("the game has been reset")
+        self.update_score()
+    def update_score(self):
+        self.display()
+        self.special()
+    def player1_scored(self):
+        self.score1 += 1
+        self.chain1 += 1
+        self.chain2 = 0
+        self.update_score()
+    def player2_scored(self):
+        self.score2 += 1
+        self.chain2 += 1
+        self.chain1 = 0
+        self.update_score()
+    def display(self):
+        self.tm.numbers(self.score1, self.score2)
+    def special(self):
+        if(self.score1 == self.maxScore and self.score2 < (self.maxScore -1)):
+            print("Home team wins")
+            self.reset()
+        elif(self.score2 == self.maxScore and self.score1 < (self.maxScore -1)):
+            print("Away team wins")
+            self.reset()
+        elif(self.score1 > (self.maxScore -self.winby) and self.score1 == self.score2):
+            print("deuce")
+        elif(self.score1 == self.score2 + self.winby and self.score2 > (self.maxScore -self.winby)):
+            print("Home team wins")
+            self.reset()
+        elif(self.score2 == self.score1 + self.winby and self.score1 > (self.maxScore -self.winby)):
+            print("Away team wins")
+            self.reset()
+        elif(self.score1 == self.score2 + 1 and self.score2 > (self.maxScore -self.winby)):
+            print("Advantege Home team")
+        elif(self.score2 == self.score1 + 1 and self.score1 > (self.maxScore -self.winby)):
+            print("Advantege Away team")
+        elif(self.score1 == self.score2):
+            print("tie game")
+        elif(self.score1 == (self.maxScore -1)):
+            print("Home team is about to win")
+        elif(self.score2 == (self.maxScore -1)):
+            print("Away team is about to win")
+        elif(self.score1 > self.score2 + 10 and self.chain1 != 0):
+            print("Home team is destroying Away team")
+        elif(self.score2 > self.score1 + 10 and self.chain2 != 0):
+            print("Away team is destroying Home team")
+        if(self.score1 > self.score2 + self.winby and self.chain2 == 5):
+            print("Away team is on come back")
+        elif(self.score2 > self.score1 + self.winby and self.chain1 == 5):
+            print("Home team is on come back")
+        if(self.chain1 == 3):
+            print("Home team is heating up")
+        elif(self.chain2 == 3):
+            print("Away team is heating up")
+        elif(self.chain1 == 5):
+            print("Home team is on fire")
+        elif(self.chain2 == 5):
+            print("Away team is on fire")
+        elif(self.chain1 == 7):
+            print("Home team needs to be stoped")
+        elif(self.chain2 == 7):
+            print("Away team needs to be stoped")
+        
 print("Welcome to the national basement ping pong competion\n")
 
 pin2 = Pin(2, Pin.OUT)
-#pin2 = Signal(2, Pin.OUT, inverted=True)
+#pin2 = Signal(self.winby, Pin.OUT, inverted=True)
 
 def main():
-    score1 = 0
-    score2 = 0
-    chain1 = 0
-    chain2 = 0
-
+    sb = Scoreboard(21 , 2)
+    
     while True:
-        display(score1, score2)
-        player1_scored(score1, score2, chain1, chain2)
-        player2_scored(score1, score2, chain1, chain2)
-        if score1 >= 21 or score2 >= 21:
-            reset()
+        sb.display()
+        sb.player1_scored()
+        sb.player2_scored()
+        
     
 
 
